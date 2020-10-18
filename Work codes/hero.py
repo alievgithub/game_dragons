312dfb06bce42e5fd8f3d7aad3fd6054f24203eb
Artem Aliev <artem.aliev@phystech.edu>
2020-10-18 22:16:33 +0300

    Fixed hero.py

--- Work codes/hero.py
+++ Work codes/hero.py
@@ -8,3 +8,13 @@ from gameunit import *
 Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
 
 """
+
+class Hero(Attacker):
+    def __init__(self, name):
+        self._health = 100
+        self._attack = 50
+        self._experience = 0
+        self._name = name
+
+    def attack(self, target):
+        target._health -= self._attack
